e225b1b43639ef2c6b07edad7ea98050d9d40517
Lukasz Paul <81487824+lukaszpaul@users.noreply.github.com>
2022-12-21 16:15:48 -0500

    Update grand.py

--- grand.py
+++ grand.py
@@ -39,7 +39,7 @@ with lookup:
     if st.button("Search"):
      result = search_csv(search_term)
      st.write(result)
-     st.write("--")
+     st.markdown('#')
     
 with dataset:
     st.markdown("<h3 style='color:rgb(0, 77, 187); font-weight:bold';>Raw Bandage Data</h3>", unsafe_allow_html=True)
